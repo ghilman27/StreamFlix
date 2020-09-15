7fc0e632afffcc56a8dfa0c803927fcce112b25c
ghilaman27 <ghilman27@gmail.com>
2020-09-16 03:24:08 +0700

    Add scrolling behaviour to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, Link } from '@material-ui/core';
 import useStyles from './NavBar.styles';
+import cx from 'classnames';
 
 const NavBar = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
+	const handleScroll = () => {
+		window.pageYOffset ? setShowMenu(true) : setShowMenu(false);
+	};
+
+	useEffect(() => {
+		// componentDidMount
+		window.addEventListener('scroll', handleScroll);
+
+		// componentWillUnmount
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	const styles = useStyles();
 
 	return (
-		<AppBar position='relative' color='primary' className={styles.root}>
+		<AppBar
+			position='fixed'
+			color='primary'
+			className={cx({
+				[styles.root]: true,
+				[styles.navHidden]: !showMenu,
+			})}
+		>
 			<Toolbar>
 				<Link
 					className={styles.appLogo}
 					href='#'
 					color='secondary'
-					variant='h5'
+					variant='h4'
 					underline='none'
 				>
-					StreamFLIX
+					StreamFlix
 				</Link>
 			</Toolbar>
 		</AppBar>
--- src/components/NavBar/NavBar.styles.js
+++ src/components/NavBar/NavBar.styles.js
@@ -3,7 +3,14 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
 	root: {
 		flexDirection: 'row',
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(1),
+		transition: 'all 0.5s ease-in-out',
 	},
 	appLogo: {},
+	navHidden: {
+		background: 'transparent',
+		boxShadow: theme.shadows[0],
+	},
 }));
