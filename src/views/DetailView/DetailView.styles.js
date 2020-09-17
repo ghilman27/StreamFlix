95762c02b45faf5bbc4f4f001223917ff0e63719
ghilaman27 <ghilman27@gmail.com>
2020-09-17 15:10:41 +0700

    fix bottom footer on detail page

--- src/views/DetailView/DetailView.styles.js
+++ src/views/DetailView/DetailView.styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
 	root: {
-		marginBottom: theme.spacing(10)
+		paddingBottom: theme.spacing(24)
 	},
 	sectionContainer: {
 		paddingLeft: '5%',
